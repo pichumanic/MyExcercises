d0d4f0d6042ff4762e5ffeca2c8c1857ab147040
Asus <Asus@PICHU>
2021-10-09 20:02:32 +0200

    Projektdateien hinzufügen.

---
+++ MyExcercises.cpp
@@ -0,0 +1,12 @@
+﻿// MyExcercises.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "MyExcercises.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ MyExcercises.h
@@ -0,0 +1,8 @@
+﻿// MyExcercises.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
